19ee7c2c55378fd985887442dabf1a83b1a3dd5b
yxj0312 <yanxiajun@hotmail.com>
2020-04-03 21:38:57 +0200

    Add axios and cors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="home">
-    login form
+    <form action="#" method="post" @submit.prevent="login">
+      <div>
+        <input type="email" name="email" placeholder="email">
+      </div>
+      <div>
+        <input type="password" name="password" placeholder="password">
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000'
 export default {
   name: 'Home',
+  methods: {
+    login() {
+      axios.get('/sanctum/csrf-cookie').then(response => {
+          console.log(response)
+      });
+    }
+  },
 }
 </script>
